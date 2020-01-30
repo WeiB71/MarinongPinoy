8ce5429e5db01bf6215fa5c833fa6deca5b6c06c
Doyle <Doyle@LEXIDEV>
2020-01-30 19:21:45 +0800

    udpate

--- LexCrewing/App_Start/BundleConfig.cs
+++ LexCrewing/App_Start/BundleConfig.cs
@@ -9,6 +9,7 @@ namespace LexCrewing
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/bootstrap-input-spinner.js",
                         "~/Scripts/jquery-{version}.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
---
+++ LexCrewing/Controllers/CodeBehindController.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using HttpGetAttribute = System.Web.Http.HttpGetAttribute;
+using HttpPostAttribute = System.Web.Http.HttpPostAttribute;
+namespace LexCrewing.Controllers
+{
+    public class CodeBehindController : ApiController
+    {
+
+        [System.Web.Mvc.HttpGet]
+        public IEnumerable<dynamic> GetRank()
+        {
+            // Create a PetaPoco database object
+            var db = new PetaPoco.Database("DefaultConnection");
+
+            // Show all Branches
+            var b = db.Query<dynamic>("SELECT RankCode,RankDescription FROM Rank").ToList();
+
+            return b;
+        }
+    }
+}
